45603c0d80f3e8b1966bfa92b7b1536b5d613809
helix-dan <lotus.bat@gmail.com>
2013-02-20 11:11:40 +0800

    can start server

---
+++ tags/mi7-myself/server.js
@@ -0,0 +1,19 @@
+var https = require('https');
+var fs    = require('fs');
+var url   = require('url');
+var _     = require('underscore');
+
+
+var option;
+
+
+
+server = https.createServer(function(request, response){
+
+
+});
+
+
+server.listen(3104);
+
+console.log("success to start the server")
